72999d7b7057c06b191f0940702b36c2e5180b6c
Prashanta D Acharya <apras.dixit@gmail.com>
2024-06-26 22:47:00 +0545

    Added Tabs component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,8 @@
 import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Tabs from "./Tabs";
+import { ProjectDetails } from "../constants/index";
 
-gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(ScrollTrigger);
 
 const Projects = () => {
@@ -13,16 +12,12 @@ const Projects = () => {
 			className="w-[80%] mx-auto py-20 flex flex-col items-center justify-center"
 		>
 			<div>
-				<h1
-					className="text-4xl font-bold mb-16
-				
-				"
-				>
+				<h1 className="text-4xl font-bold mb-16">
 					My Creative <span className="text-[#0091ff]">Portfolio</span>
 				</h1>
 			</div>
 			<div>
-				<Tabs />
+				<Tabs projects={ProjectDetails} />
 			</div>
 		</main>
 	);
