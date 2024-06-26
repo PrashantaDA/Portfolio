a58ad0d89b9613d744b9a0d033b225204591dcd0
Prashanta D Acharya <apras.dixit@gmail.com>
2024-06-26 22:31:06 +0545

    For easy import of images

---
+++ public/index.jsx
@@ -0,0 +1,8 @@
+import Profile from "./assets/dxb-.jpg";
+import { Logo } from "./assets/Logo.png";
+
+import { Movie } from "./assets/sites/Movie.png";
+import { Store } from "./assets/sites/Store.png";
+import { YTube } from "./assets/sites/YTube.png";
+
+export { Profile, Logo, Movie, Store, YTube };
