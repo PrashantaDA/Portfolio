a198bbc7c0aef60c9ec82e4517119f1567532963
Prashanta D Acharya <apras.dixit@gmail.com>
2024-06-26 23:28:27 +0545

    Added styles

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,12 +11,12 @@ const Projects = () => {
 			id="projects"
 			className="w-[80%] mx-auto py-20 flex flex-col items-center justify-center"
 		>
-			<div>
+			<div className="w-full text-center">
 				<h1 className="text-4xl font-bold mb-16">
 					My Creative <span className="text-[#0091ff]">Portfolio</span>
 				</h1>
 			</div>
-			<div>
+			<div className="w-full">
 				<Tabs projects={ProjectDetails} />
 			</div>
 		</main>
