6d690601abb670cd0371d3f1354cec1dd0fc61dc
Prashanta D Acharya <apras.dixit@gmail.com>
2024-06-28 11:28:31 +0545

    Styles updated

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -2,7 +2,7 @@ import { BsFacebook, BsTwitterX, BsInstagram, BsGithub } from "react-icons/bs";
 
 const SocialIcons = () => {
 	return (
-		<div className="xs:hidden md:flex flex-col items-start justify-center h-screen fixed md:px-8 gap-4 left-0 top-10 z-50 xs:px-4">
+		<div className="xs:hidden sm:flex flex-col items-start justify-center h-screen fixed md:px-8 gap-4 left-0 top-10 z-50 xs:px-4">
 			<a
 				className="text-[#0866FF] social-icon"
 				href="https://www.facebook.com/PrasDixit/"
