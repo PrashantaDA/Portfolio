3937b876208a71d9de3ea1620bd9bc3aebd46479
Prashanta D Acharya <apras.dixit@gmail.com>
2024-10-01 18:08:53 +0545

    feat: update project images

--- src/images/index.jsx
+++ src/images/index.jsx
@@ -1,12 +1,10 @@
 import Movie from "./sites/Movie.png";
 import Store from "./sites/Store.png";
 import YTube from "./sites/YTube.png";
-import Gpt from "./sites/Gpt.png";
 import Restro from "./sites/Restro.png";
-import Bank from "./sites/Bank.png";
 import Pf3d from "./sites/Pf3d.png";
 import Poke from "./sites/Poke.png";
 import Rexipe from "./sites/Rexipe.png";
 import Hotelix from "./sites/Hotelix.png";
 
-export { Movie, Store, YTube, Restro, Bank, Gpt, Pf3d, Poke, Rexipe, Hotelix };
+export { Movie, Store, YTube, Restro, Pf3d, Poke, Rexipe, Hotelix };
